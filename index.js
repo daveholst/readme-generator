dcf23ac599e14ac0b4662468e15b3fb1057139d8
Dave Holst <daveholst@gmail.com>
2021-04-30 14:41:13 +0800

    added basic quiestions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const genMD = require('./utils/generateMarkdown.js')
+const genMd = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -10,15 +10,42 @@ const questions = [
     message: 'What is the name of this project?'
   },
   {
-
+    type: 'editor',
+    name: 'description',
+    message: 'What is the the project description?'
+  },
+  {
+    type: 'editor',
+    name: 'installation',
+    message: 'What are the installation notes?'
+  },
+  {
+    type: 'editor',
+    name: 'usage',
+    message: 'How is the project to be used?'
+  },
+  {
+    type: 'editor',
+    name: 'contribution',
+    message: 'How can users contribute to the project?'
+  },
+  {
+    type: 'editor',
+    name: 'tests',
+    message: 'What are porject tests?'
   }
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+}
 
 // Function call to initialize app
 init();
