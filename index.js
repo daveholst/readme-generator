dda0e310fe3e6e00407fe29edbc031e528668850
Dave Holst <daveholst@gmail.com>
2021-05-01 11:11:10 +0800

    basic generation of MD string including ToC

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const genMd = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
@@ -10,41 +11,73 @@ const questions = [
     message: 'What is the name of this project?'
   },
   {
-    type: 'editor',
+    type: 'list',
+    name: 'license',
+    message: 'What type of license would you like?',
+    choices: ['MIT', 'Apache', 'GPL', 'None']
+  },
+  {
+    type: 'input',
+    name: 'githubUser',
+    message: 'What is your GitHub username?',
+    filter: function (val) {
+      return val.toLowerCase();
+    },
+  },
+  {
+    type: 'confirm',
+    name: 'toc',
+    message: 'Would you like a table of contents?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What email should users contact you on?'
+  },
+  {
+    type: 'input',
     name: 'description',
     message: 'What is the the project description?'
   },
   {
-    type: 'editor',
+    type: 'input',
     name: 'installation',
     message: 'What are the installation notes?'
   },
   {
-    type: 'editor',
+    type: 'input',
     name: 'usage',
     message: 'How is the project to be used?'
   },
   {
-    type: 'editor',
+    type: 'input',
     name: 'contribution',
     message: 'How can users contribute to the project?'
   },
   {
-    type: 'editor',
+    type: 'input',
     name: 'tests',
     message: 'What are porject tests?'
+  },
+  {
+    type: 'confirm',
+    name: 'questions',
+    message: 'Do you want to include a questions section?'
   }
-
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {
-  inquirer.prompt(questions).then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
-  });
+async function init() {
+  let responses = await inquirer.prompt(questions);
+  // responses = JSON.stringify(responses, null, '  ');
+  console.log(responses);
+  const markdown = generateMarkdown(responses);
+
+  console.log(markdown);
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,9 +10,33 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+// function to build toc based on user preferences
+function tableOfContents(data) {
+  if (data.toc) {
+    // create heading
+    let tocData = `## Table of Contents:\n`;
+    // check if license data to be included
+    if (data.license !== 'none') {
+      tocData += `* [License](#License)\n`;
+    };
+    // check installation data
+    tocData += data.installation ? `* [Installation](#Installation)\n` : '';
+    tocData += data.usage ? `* [Usage](#Usage)\n` : '';
+    tocData += data.tests ? `* [Tests](#Tests)\n` : '';
+    tocData += data.contributing ? `* [Contributing](#Contributing)\n` : '';
+    tocData += data.questions ? `* [Questions](#Questions)\n` : '';
+    return tocData;
+  } else {
+    return '';
+  };
+}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  return `
+  # ${data.projectTitle}
+  ${(tableOfContents(data))}
 
 `;
 }
